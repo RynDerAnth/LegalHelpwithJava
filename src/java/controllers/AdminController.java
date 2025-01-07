956c89e87730dd6427456cc1f669974f550e41b1
Rayyn <166891366+RynDerAnth@users.noreply.github.com>
2025-01-07 13:50:19 +0700

    Update AdminController.java

--- src/java/controllers/AdminController.java
+++ src/java/controllers/AdminController.java
@@ -215,6 +215,7 @@ public class AdminController extends HttpServlet {
             int id = Integer.parseInt(request.getParameter("id"));
             consultantModel.setId(id);
             consultantModel.delete();
+            next = "admin?menu=pView";
             
         } else if ("addA".equals(action)) {
             String picture_path = request.getParameter("picture_path");
